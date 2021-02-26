4a314ba5978e84a32d154898520de8ad76267b63
Christian Cesar <37115113+christiancesar@users.noreply.github.com>
2021-02-26 18:23:49 -0400

    Add router get/post serveys

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -1,8 +1,27 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRespository } from "../repositories/SurveysRepository";
 class SurveysController {
     async create(request: Request, response: Response) {
         const { title, description } = request.body;
+
+        const surveysRespository = getCustomRepository(SurveysRespository);
+
+        const survey = surveysRespository.create({
+            title,
+            description
+        });
+
+        await surveysRespository.save(survey);
+
+        return response.status(201).json(survey);
+    }
+
+    async show(request: Request, response: Response) {
+        const surveysRespository = getCustomRepository(SurveysRespository);
+        const all = await surveysRespository.find();
+        return response.json(all);
     }
 }
 
-export { SurveysController }
+export { SurveysController };
--- src/database/migrations/1614371549278-CreateSurveys.ts
+++ src/database/migrations/1614371549278-CreateSurveys.ts
@@ -21,7 +21,7 @@ export class CreateSurveys1614371549278 implements MigrationInterface {
                             type: "varchar"
                         },
                         {
-                            name: "create_at",
+                            name: "created_at",
                             type: "timestamp",
                             default: "now()"
                         }
